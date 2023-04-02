7e6dd0a237b6d87b8ea8a4dd5168af084e1937c2
Aaron Kuo <kuoaaron4398@gmail.com>
2023-04-02 11:35:48 -0700

    ok but how retarded am i

--- src/App.js
+++ src/App.js
@@ -4,49 +4,49 @@ import  { Amplify, API } from 'aws-amplify'
 import React, { useEffect, useState } from 'react'
 
 const myAPI = "apic6cb0264"
-const path = '/customers';
+const path = '/customer';
 
 const App = () => {
-  const [input, setInput] = useState("")
-  const [customers, setCustomers] = useState([])
+    const [input, setInput] = useState("")
+    const [customers, setCustomers] = useState([])
 
-  //Function to fetch from our backend and update customers array
-  function getCustomer(e) {
-    let customerId = e
-    API.get(myAPI, path + "/" + customerId)
-        .then(response => {
-          console.log(response)
-          let newCustomers = [...customers]
-          newCustomers.push(response)
-          setCustomers(newCustomers)
+    //Function to fetch from our backend and update customers array
+    function getCustomer(e) {
+        let customerId = e.input
+        API.get(myAPI, path + "/" + customerId)
+            .then(response => {
+                console.log(response)
+                let newCustomers = [...customers]
+                newCustomers.push(response)
+                setCustomers(newCustomers)
 
-        })
-        .catch(error => {
-          console.log(error)
-        })
-  }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
-  return (
+    return (
 
-      <div className="App">
-        <h1>Super Simple React App</h1>
-        <div>
-          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <div className="App">
+            <h1>Super Simple React App</h1>
+            <div>
+                <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+            </div>
+            <br/>
+            <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+            <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+            {
+                customers.map((thisCustomer, index) => {
+                    return (
+                        <div key={thisCustomer.customerId}>
+                            <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+                        </div>)
+                })
+            }
         </div>
-        <br/>
-        <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
-
-        <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
-        {
-          customers.map((thisCustomer, index) => {
-            return (
-                <div key={thisCustomer.customerId}>
-                  <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
-                </div>)
-          })
-        }
-      </div>
-  )
+    )
 }
 
 export default App;
